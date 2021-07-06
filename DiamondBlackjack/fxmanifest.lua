64533c04c93feb066bbb8bf06f5ab860c5883401
dug <etan@ctemplar.com>
2021-07-06 13:55:48 +0100

    update fxmanifest

--- DiamondBlackjack/fxmanifest.lua
+++ DiamondBlackjack/fxmanifest.lua
@@ -1,4 +1,4 @@
-fx_version 'adamant'
+fx_version 'cerulean'
 
 game "gta5"
 
@@ -21,33 +21,13 @@ client_scripts {
 
 server_script "sv_blackjack.lua"
 
-files{
+files {
 	"peds.meta",
-	'audio/dlcvinewood_amp.dat10',
-	'audio/dlcvinewood_amp.dat10.nametable',
-	'audio/dlcvinewood_amp.dat10.rel',
-	'audio/dlcvinewood_game.dat151',
-	'audio/dlcvinewood_game.dat151.nametable',
-	'audio/dlcvinewood_game.dat151.rel',
-	'audio/dlcvinewood_mix.dat15',
-	'audio/dlcvinewood_mix.dat15.nametable',
-	'audio/dlcvinewood_mix.dat15.rel',
-	'audio/dlcvinewood_sounds.dat54',
-	'audio/dlcvinewood_sounds.dat54.nametable',
-	'audio/dlcvinewood_sounds.dat54.rel',
-	'audio/dlcvinewood_speech.dat4',
-	'audio/dlcvinewood_speech.dat4.nametable',
-	'audio/dlcvinewood_speech.dat4.rel',
-	'audio/sfx/dlc_vinewood/casino_general.awc',
-	'audio/sfx/dlc_vinewood/casino_interior_stems.awc',
-	'audio/sfx/dlc_vinewood/casino_slot_machines_01.awc',
-	'audio/sfx/dlc_vinewood/casino_slot_machines_02.awc',
-	'audio/sfx/dlc_vinewood/casino_slot_machines_03.awc',
-	'audio/sfx/dlc_vinewood/*.awc',
+	'audio/*.*',
+	'audio/**/*.*',
 }
 
 data_file "PED_METADATA_FILE" "peds.meta"
-
 data_file 'AUDIO_GAMEDATA' 'audio/dlcvinewood_game.dat'
 data_file 'AUDIO_SOUNDDATA' 'audio/dlcvinewood_sounds.dat'
 data_file 'AUDIO_DYNAMIXDATA' 'audio/dlcvinewood_mix.dat'
